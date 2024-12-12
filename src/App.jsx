cfbbed578fbf6957bdf7509f796c4a57e52c543c
Garry West <garrywest@msn.com>
2024-12-12 17:40:08 +0000

    Combine useEffect and useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Welcome from './components/Welcome';
 import Support from './components/Support';
+import ListCast from './components/ListCast';
 
 function App() {
   const name = 'StarGazers'
@@ -13,6 +14,7 @@ function App() {
           <p>Members of an <b>intergalactic alliance</b><br />
             paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
           <Support />
+          <ListCast />
         </hgroup>
       </article>
     </div>
