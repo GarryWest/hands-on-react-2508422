956418d7ed86241ffe967010a77acf26c5f1ce98
Garry West <garrywest@msn.com>
2024-12-13 14:45:00 +0000

    Custom event triggers and modals

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Welcome from './components/Welcome';
 import Support from './components/Support';
 import ListCast from './components/ListCast';
+import Modals from './components/Modals';
 
 function App() {
   const name = 'StarGazers'
+  let [memberInfo, setMemberInfo] = useState(null);
   return (
     <div className="container">
-      <article>
-        <hgroup>
+      <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
           <Welcome name="StarGazers" />
           <p>Members of an <b>intergalactic alliance</b><br />
             paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
           <Support />
-          <ListCast />
-        </hgroup>
-      </article>
+          <ListCast onChoice={ (info) => {setMemberInfo(info)}} />
+          { memberInfo && <Modals memberInfo={memberInfo} handleClose={() => {setMemberInfo(null)}} />}   
+      </hgroup>         
     </div>
   )
 }
